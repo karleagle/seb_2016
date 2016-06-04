7d10efd044d394b46e34c7445019f4ebd3b2cd8c
karleagle <karleagle@gmail.com>
2016-06-04 12:39:10 +1200

    Templates

---
+++ footer.php
@@ -0,0 +1,47 @@
+	<a href="#" class="scrollToTop fixedScroll">
+    BACK TO TOP
+  </a>
+  <footer>
+
+    <nav class="footer">
+			<div>
+
+				<ul>
+					<li>
+						<a href="mail&#116;&#111;&#58;enquir&#37;69&#37;&#54;&#53;%73&#64;s%61ll%79%&#54;5%61gl&#37;65&#46;%&#54;&#51;o&#46;n%7&#65;?subject=Website%20enquiry" id="email">EMAIL</a>
+					</li>
+					<li>
+						<a href="https://www.facebook.com/sallyeaglebridal" id="facebook" target="_blank">FACEBOOK</a>
+					</li>
+					<li>
+						<a href="http://pinterest.com/sallyeagle16/" id="pinterest" target="_blank">PINTEREST</a>
+					</li>
+					<li>
+						<a href="https://www.instagram.com/sally_eagle_bridal/" id="instagram" target="_blank">INSTAGRAM</a>
+					</li>
+				</ul>
+				<h4 class="blue">
+					&copy; SALLY EAGLE BRIDAL LTD 2016&nbsp;&nbsp;|&nbsp;&nbsp;ALL RIGHTS RESERVED
+				</h4>
+			</div>
+		</nav>
+	</footer>
+	<!-- PIWIK -->
+	<script type="text/javascript">
+	  var _paq = _paq || [];
+	  _paq.push(['trackPageView']);
+	  _paq.push(['enableLinkTracking']);
+	  (function() {
+		var u=(("https:" == document.location.protocol) ? "https" : "http") + "://sallyeagle.co.nz/analytics//";
+		_paq.push(['setTrackerUrl', u+'piwik.php']);
+		_paq.push(['setSiteId', 1]);
+		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+		g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+	  })();
+
+	// stop menu anchors running
+	$("a.dropdown").click(function(event){
+	  event.preventDefault();
+	});
+	</script>
+	<noscript><p><img src="http://sallyeagle.co.nz/analytics/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
---
+++ header.php
@@ -0,0 +1,62 @@
+	<header class="opaque88">
+		<nav class="main">
+			<ul class="main">
+				<li class="underline">
+					<a class="dropdown under" href="index.php?view=g">BROWSE</a>
+					<ul class="opaque88">
+<!-- UNCOMMENT WHEN 'ALL DRESSES' SECTION PROPERLY FORMATTED
+						<li>
+							<a href="index.php?view=g">ALL DRESSES</a>
+						</li>
+-->
+						<li>
+							<a href="index.php?view=collection">BRIDAL</a>
+						</li>
+						<li>
+							<a href="index.php?view=bridesmaids">BRIDESMAIDS</a>
+						</li>
+						<li>
+							<a href="index.php?view=flowergirls">FLOWER GIRLS</a>
+						</li>
+						<li>
+							<a href="index.php?view=realbrides">REAL BRIDES</a>
+						</li>
+						<li>
+							<a href="index.php?view=accessories">ACCESSORIES</a>
+						</li>
+					</ul>
+				</li>
+				<li class="underline">
+					<a class="dropdown" href="index.php?view=s">VISIT</a>
+					<ul class="opaque88">
+						<li>
+							<a href="index.php?view=boutique">THE BOUTIQUE</a>
+						</li>
+						<li>
+							<a href="index.php?view=stockists">STOCKISTS</a>
+						</li>
+						<li>
+							<a href="index.php?view=stockists">TRUNK SHOWS</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a class="home_img" href="index.php"><img src="images/icons/logo.png" id="logo" alt="Home"/></a>
+				</li>
+				<li class="underline">
+					<a class="dropdown" href="index.php?view=a">ABOUT</a>
+					<ul class="opaque88 about">
+						<li>
+							<a href="index.php?view=a">SALLY EAGLE</a>
+						</li>
+						<li>
+							<a href="index.php?view=process">THE PROCESS</a>
+						</li>
+					</ul>
+				</li>
+				<li class="underline">
+					<a href="index.php?view=o">CONTACT</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
