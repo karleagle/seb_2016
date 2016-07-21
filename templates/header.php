f2501281d640da03233294654c611814b93447cd
karleagle <karleagle@gmail.com>
2016-07-21 14:01:54 +1200

    Update 20160721

--- templates/header.php
+++ templates/header.php
@@ -1,61 +1,64 @@
 	<header class="opaque88">
 		<nav class="main">
 			<ul class="main">
-				<li class="underline">
-					<a class="dropdown under" href="index.php?view=g">BROWSE</a>
-					<ul class="opaque88">
+				<li class="underline dropdown">
+					<a class=" under" href="index.php?view=gallery">BROWSE</a>
+					<ul class="opaque88 hidden">
 <!-- UNCOMMENT WHEN 'ALL DRESSES' SECTION PROPERLY FORMATTED
 						<li>
 							<a href="index.php?view=g">ALL DRESSES</a>
 						</li>
 -->
 						<li>
-							<a href="index.php?view=collection">BRIDAL</a>
+							<a href="index.php?view=gallery&category=bridal">BRIDAL</a>
 						</li>
 						<li>
-							<a href="index.php?view=bridesmaids">BRIDESMAIDS</a>
+							<a href="index.php?view=gallery&category=realbrides">REAL BRIDES</a>
 						</li>
 						<li>
-							<a href="index.php?view=flowergirls">FLOWER GIRLS</a>
+							<a href="index.php?view=gallery&category=bridesmaids">BRIDESMAIDS</a>
 						</li>
 						<li>
-							<a href="index.php?view=realbrides">REAL BRIDES</a>
+							<a href="index.php?view=gallery&category=flowergirls">FLOWER GIRLS</a>
 						</li>
 						<li>
-							<a href="index.php?view=accessories">ACCESSORIES</a>
+							<a href="index.php?view=gallery&category=accessories">ACCESSORIES</a>
 						</li>
 					</ul>
 				</li>
-				<li class="underline">
-					<a class="dropdown" href="index.php?view=s">VISIT</a>
-					<ul class="opaque88">
+				<li class="underline dropdown">
+					<a class="" href="index.php?view=boutique">VISIT</a>
+					<ul class="opaque88 hidden">
 						<li>
-							<a href="index.php?view=boutique">THE BOUTIQUE</a>
+							<a href="index.php?view=boutique">WELLINGTON</a>
 						</li>
+							<li>
+								<a href="index.php?view=auckland">AUCKLAND</a>
+							</li>
 						<li>
 							<a href="index.php?view=stockists">STOCKISTS</a>
 						</li>
 						<li>
-							<a href="index.php?view=stockists">TRUNK SHOWS</a>
+							<a href="index.php?view=trunkshows">TRUNK SHOWS</a>
 						</li>
 					</ul>
 				</li>
 				<li>
 					<a class="home_img" href="index.php"><img src="images/icons/logo.png" id="logo" alt="Home"/></a>
 				</li>
-				<li class="underline">
-					<a class="dropdown" href="index.php?view=a">ABOUT</a>
-					<ul class="opaque88 about">
+				<li class="underline dropdown about_menu">
+					<a class="" href="index.php?view=about">ABOUT</a>
+					<ul class="opaque88 about hidden">
 						<li>
-							<a href="index.php?view=a">SALLY EAGLE</a>
+							<a href="index.php?view=about">SALLY EAGLE</a>
 						</li>
 						<li>
 							<a href="index.php?view=process">THE PROCESS</a>
 						</li>
 					</ul>
 				</li>
-				<li class="underline">
-					<a href="index.php?view=o">CONTACT</a>
+				<li class="underline no_sub">
+					<a href="index.php?view=contact">CONTACT</a>
 				</li>
 			</ul>
 		</nav>
