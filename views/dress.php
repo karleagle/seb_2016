fc90ecf9faa1057511dafc71d87411af812705ea
karleagle <karlwaats@hotmail.com>
2016-07-31 07:35:58 +1200

    Initialise repo

---
+++ views/dress.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+<meta charset="UTF-8">
+<title>Wedding Dress from Sally Eagle Bridal&#39;s Collection</title>
+<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+<meta name="description" content="Wedding dress by Sally Eagle Bridal" />
+<meta name="keywords" content="sally eagle, sally eagle bridal, vintage, vintage inspired, vintage wedding dress, vintage wedding gown, vintage bridal gown, wedding dress, wedding dresses, bridal dress, bridal dresses, bridal gown, bridal gowns, bridal wear, bridalwear, wedding dress designer, wedding dress designers, bridal gown designer, bridal gown designers, bridal dress design, bridal dress designer, bridal design, bridal designer, bridal designers, designer bridal, bridal dressmaker, wedding gown designer, wedding gown designers, wellington, willis st, willis street, bridal shop, wellington bridal shop, bridal store, wellington bridal, wellington bridal store, bridal boutique, wellington bridal boutique, wedding dress shop, wedding dress store, wedding dress boutique, wedding gown store, wedding gown shop, wedding gown boutique, bridesmaid, bridesmain dresses, bridesmaids dresses, ball dress, ball dresses, ball dress shops" />
+<meta name="robots" content="INDEX,FOLLOW" />
+
+
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<script src="https://use.typekit.net/vyq5cic.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/menu_hover.js"></script>
+
+<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+<link rel="icon" type="image/ico" href="images/favicon.ico"/>
+</head>
+
+<body>
+<?php // INCLUDE HEADER
+	include('templates/header.php');
+?>
+  <div class="content left_align dresses">
+    <div class="image_container">
+      <div class="image_thumbs">
+				<div class="overlay">
+        	<img class="dress thumb" src="images/collections/wedding_45.jpg"
+					alt="Sally Eagle's 2017 wedding dress and bridal wear collection shoot" />
+				</div>
+				<div class="overlay">
+        	<img class="dress thumb" src="images/collections/wedding_45.jpg"
+					alt="Sally Eagle's 2017 wedding dress and bridal wear collection shoot" />
+				</div>
+				<div class="overlay">
+        	<img class="dress thumb" src="images/collections/wedding_45.jpg"
+					alt="Sally Eagle's 2017 wedding dress and bridal wear collection shoot" />
+				</div>
+      </div>
+    <img class="dress main" src="images/collections/wedding_45.jpg" alt="Sally Eagle's 2015/2016 wedding dress and bridal wear collection shoot" />
+    </div>
+		<div>
+	    <a class="lt_grey small ls_wide" href='index.php?view=gallery'>
+	      BACK TO VIEW ALL
+	    </a>
+		</div>
+    <h1 class="blue underscore dress_name">
+      AUDREY
+    </h1>
+    <p>
+      Audrey Hepburn was everybody&#39;s dream girl. The Audrey dress speaks of her glamour and sophisticated style. The dress is made from silk georgette that gently falls over the hips creating fullness at the hem. While Audrey is known for wearing simple shaped, elegant dresses she always glammed them up with a bit of sparkle and personality. The delicate beading on the bodice bring that hint of sparkle, while the bow on the shoulder drapes down giving the dress that feminine touch.
+    </p>
+    <a class="box enquire ls_wide" href='mail&#116;&#111;&#58;enquir&#37;69&#37;&#54;&#53;%73&#64;s%61ll%79%&#54;5%61gl&#37;65&#46;%&#54;&#51;o&#46;n%7&#65;?subject=Dress%20enquiry%20-%20AUDREY'>
+      ENQUIRE NOW
+    </a>
+    <!-- FABRIC CONTENT AND DRESS DETAILS -->
+    <section class="details box">
+      <h3 class="dk_grey">
+        FABRIC CONTENT
+      </h3>
+      <p>
+        <span class="blue">Outer:</span> beaded lace, silk georgette; <span class="blue">Lining:</span> satin. Dry Clean only.
+      </p>
+      <h3 class="dk_grey margin_top">
+        DRESS DETAILS
+      </h3>
+      <p>
+        All Sally Eagle&#39;s collection wedding dresses are available in sizes 6 through to 16, with the hem length to be determined by fitting.
+      </p>
+      <p>
+        To ensure a perfect fit you can also have your dress made and fitted to your exact measurements at the Sally Eagle Bridal boutique in Wellington, New Zealand.
+      </p>
+      <p>
+        Proudly Made in New Zealand.
+      </p>
+    </section>
+    <!-- END MAIN BLOCK -->
+    </div>
+  <!-- END CONTENT BLOCK -->
+  </div>
+	<!-- START SIMILAR STYLES -->
+	<section class="similar_styles">
+		<div class="titlebox">
+			<h2>SIMILAR STYLES</h2>
+		</div>
+		<div class="border styles_gallery">
+			<div class="gallery_slide">
+				<img class="" src="images/collections/wedding_52.jpg" alt="Sally Eagle's 2015/2016 wedding dress and bridal wear collection shoot">
+				<img class="" src="images/collections/wedding_52.jpg" alt="Sally Eagle's 2015/2016 wedding dress and bridal wear collection shoot">
+				<img class="" src="images/collections/wedding_52.jpg" alt="Sally Eagle's 2015/2016 wedding dress and bridal wear collection shoot">
+				<img class="" src="images/collections/wedding_52.jpg" alt="Sally Eagle's 2015/2016 wedding dress and bridal wear collection shoot">
+				<img class="" src="images/collections/wedding_52.jpg" alt="Sally Eagle's 2015/2016 wedding dress and bridal wear collection shoot">
+				<img class="" src="images/collections/wedding_52.jpg" alt="Sally Eagle's 2015/2016 wedding dress and bridal wear collection shoot">
+				<img class="" src="images/collections/wedding_52.jpg" alt="Sally Eagle's 2015/2016 wedding dress and bridal wear collection shoot">
+				<img class="" src="images/collections/wedding_52.jpg" alt="Sally Eagle's 2015/2016 wedding dress and bridal wear collection shoot">
+			</div>
+		</div>
+	</section>
+	<!-- END SIMILAR STYLES -->
+<?php // INCLUDE FOOTER
+	include('templates/footer.php');
+?>
+</body>
+
+</html>
---
+++ views/trunkshows.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+<meta charset="UTF-8">
+<title>Sally Eagle presents Lan & Emmerson</title>
+<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+<meta name="description" content="See a Sally Eagle wedding dress, as worn at Lan & Emmerson's wedding" />
+<meta name="keywords" content="sally eagle, sally eagle bridal, vintage, vintage inspired, vintage wedding dress, vintage wedding gown, vintage bridal gown, wedding dress, wedding dresses, bridal dress, bridal dresses, bridal gown, bridal gowns, bridal wear, bridalwear, wedding dress designer, wedding dress designers, bridal gown designer, bridal gown designers, bridal dress design, bridal dress designer, bridal design, bridal designer, bridal designers, designer bridal, bridal dressmaker, wedding gown designer, wedding gown designers, wellington, willis st, willis street, bridal shop, wellington bridal shop, bridal store, wellington bridal, wellington bridal store, bridal boutique, wellington bridal boutique, wedding dress shop, wedding dress store, wedding dress boutique, wedding gown store, wedding gown shop, wedding gown boutique, bridesmaid, bridesmain dresses, bridesmaids dresses, ball dress, ball dresses, ball dress shops" />
+<meta name="robots" content="INDEX,FOLLOW" />
+
+
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<script src="https://use.typekit.net/vyq5cic.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/menu_hover.js"></script>
+
+<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+<link rel="icon" type="image/ico" href="images/favicon.ico"/>
+</head>
+
+<body>
+<?php // INCLUDE HEADER
+	include('templates/header.php');
+?>
+	<div id="1" class="submenu ">
+		<ul class="opaque88">
+			<li>
+				<a href="#2">AUGUST 2016</a>
+			</li>
+			<li>
+				<a href="#3">SEPTEMBER 2016</a>
+			</li>
+			<li>
+				<a href="#4">OCTOBER 2016</a>
+			</li>
+			<li>
+				<a href="#5">NOVEMBER 2016</a>
+			</li>
+		</ul>
+	</div>
+  <h2 class="stockist">
+    JULY 2016
+  </h2>
+  <div class='bar'><p>&#95;&#95;&#95;&#95;</p></div>
+  <div class="stockist">
+    <h1 class="underscore stockist blue">
+      CHRISTCHURCH, NZ
+    </h1>
+		<p class="ls_wide">
+			12&#8211;13 AUGUST
+		</p>
+    <div>
+			<a class="ls_thin stockist light"
+				href="http://www.millefeuille.co.nz/?page_id=16" target="_blank">
+	      Mille Feuille
+	    </a>
+		</div>
+		<div>
+	    <a class="ls_thin black"
+				href="https://www.google.co.nz/maps/place/Mille+Feuille/@-43.5809392,172.560495,17z/data=!3m1!4b1!4m5!3m4!1s0x6d318bb59ef7ec77:0xe0bf8c6522f01881!8m2!3d-43.5809431!4d172.5626837" target='_blank'>
+				29 Nicholls Rd, Christchurch
+			</a>
+		</div>
+		<div>
+	    <a class="email dk_grey ls_thin"
+				href="mailto:info@millefeuille.co.nz">
+				info@millefeuille.co.nz
+			</a>
+		</div>
+		<p>
+			Phone <span class="dk_grey">03 356 0163</span>
+		</p>
+  </div>
+	<div class="stockist">
+    <h1 class="underscore stockist blue">
+      SYDNEY, AU
+    </h1>
+		<p class="ls_wide">
+			13&#8211;14 AUGUST
+		</p>
+    <div>
+			<a class="ls_thin stockist light"
+	      href="http://www.hopeandpage.com.au/contact-us/" target="_blank">
+	      Hope and Page
+	    </a>
+		</div>
+		<div>
+	    <a class="ls_thin black"
+	      href="https://www.google.co.nz/maps/place/Hope+X+Page/@-34.0436385,151.1222513,17z/data=!3m1!4b1!4m5!3m4!1s0x6b12c7ee5e5d3bd9:0x89fbddea08af8969!8m2!3d-34.043643!4d151.12444" target="_blank">
+	      1/296 Kingsway, Caringbah NSW 2229
+	    </a>
+		</div>
+		<div>
+	    <a class="email dk_grey ls_thin"
+	      href="contact@hopeandpage.com.au">
+	      contact@hopeandpage.com.au
+	    </a>
+		</div>
+		<p>
+			Phone <span id="2" class="dk_grey">0410 628 564</span>
+		</p>
+  </div>
+  <h2 class="stockist">
+    AUGUST 2016
+  </h2>
+  <div class='bar'><p>&#95;&#95;&#95;&#95;</p></div>
+	<div class="stockist">
+    <h1 class="underscore stockist blue"
+      href="" target="_blank">
+      KANSAS CITY, USA
+    </h1>
+		<p class="ls_wide">
+			17&#8211;18 JULY
+		</p>
+    <div>
+			<a class="ls_thin stockist light"
+	      href="" target="_blank">
+	      Something White Bridal Boutique
+	    </a>
+		</div>
+		<div>
+	    <a class="ls_thin black"
+	      href="" target="_blank">
+	      1510 Walnut St, Suite B, Kansas City, MO 64108
+	    </a>
+		</div>
+		<div>
+	    <a class="email dk_grey ls_thin"
+	      href="" target="_blank">
+	      hello@somethingwhitebridalboutique.com
+	    </a>
+		</div>
+		<p>
+			Phone <span class="dk_grey">816 216 7454</span>
+		</p>
+  </div>
+  <div class="stockist">
+    <h1 class="underscore stockist blue"
+      href="" target="_blank">
+      KANSAS CITY, USA
+    </h1>
+		<p class="ls_wide">
+			17&#8211;18 JULY
+		</p>
+    <div>
+			<a class="ls_thin stockist light"
+	      href="" target="_blank">
+	      Something White Bridal Boutique
+	    </a>
+		</div>
+		<div>
+	    <a class="ls_thin black"
+	      href="" target="_blank">
+	      1510 Walnut St, Suite B, Kansas City, MO 64108
+	    </a>
+		</div>
+		<div>
+	    <a class="email dk_grey ls_thin"
+	      href="" target="_blank">
+	      hello@somethingwhitebridalboutique.com
+	    </a>
+		</div>
+		<p>
+			Phone <span class="dk_grey">816 216 7454</span>
+		</p>
+  </div>
+  <div class="stockist">
+    <h1 class="underscore stockist blue"
+      href="" target="_blank">
+      KANSAS CITY, USA
+    </h1>
+		<p class="ls_wide">
+			17&#8211;18 JULY
+		</p>
+    <div>
+			<a class="ls_thin stockist light"
+	      href="" target="_blank">
+	      Something White Bridal Boutique
+	    </a>
+		</div>
+		<div>
+	    <a class="ls_thin black"
+	      href="" target="_blank">
+	      1510 Walnut St, Suite B, Kansas City, MO 64108
+	    </a>
+		</div>
+		<div>
+	    <a class="email dk_grey ls_thin"
+	      href="" target="_blank">
+	      hello@somethingwhitebridalboutique.com
+	    </a>
+		</div>
+		<p>
+			Phone <span class="dk_grey">816 216 7454</span>
+		</p>
+  </div>
+	<!-- END MAIN BLOCK -->
+<?php // INCLUDE FOOTER
+	include('templates/footer.php');
+?>
+</body>
+
+</html>
