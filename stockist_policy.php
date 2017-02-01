a4b624d8a1ee6e21d12bfa14e62ae849932e83f1
karleagle <karlwaats@hotmail.com>
2017-02-01 20:56:39 +1300

    Add stockist policy to website untracked

---
+++ stockist_policy.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<title>Sally Eagle Stockist Policy</title>
+	<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+	<meta name="description" content="Stockist Policy." />
+	<meta name="keywords" content="sally eagle, sally eagle bridal, stockist policy" />
+	<meta name="robots" content="NO-INDEX,NO-FOLLOW" />
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<script src="https://use.typekit.net/vyq5cic.js"></script>
+	<script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-NGJ5HZ');</script>
+	<!-- End Google Tag Manager -->
+
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/menu_hover.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+	<link rel="icon" type="image/ico" href="images/favicon.ico"/>
+	</head>
+
+<body>
+<?php include('templates/header.php'); ?>
+	<!-- START BAR -->
+	<div class="stockist">
+    <h1 class="underscore blue stockist policy">STOCKIST POLICY</h1>
+  </div>
+	<!-- END BAR -->
+	<div class="left_align padded policy">
+	  <p class="large blue">
+			Thank you for being one of our lovely stockists! We appreciate the work you do :)
+		</p>
+		<p>
+			We’ve put together a bit of information about our processes and policies so you know what we require and what to expect.
+	  </p>
+		<div class="subheading">
+			<h2 class="blue underscore">
+				ORDERING
+			</h2>
+		</div>
+	  <p>
+			Wholesale orders are placed by emailing through the order to
+				<a class="ls_thin" href="mailto:orders@sallyeagle.co.nz?subject=Stockist%20order">
+					orders@sallyeagle.co.nz</a>.
+			Provide the following information:
+		</p>
+		<ul>
+			<li>the set size based on our size chart OR the measurements;</li>
+			<li>the bride’s name;</li>
+			<li>the wedding date;</li>
+			<li>the fitting date (or earliest date required).</li>
+		</ul>
+		<p>
+			Once the order is received we will send you a quote with the details of the order for you to confirm by way of 50% deposit.
+	  </p>
+	  <p>
+			We will start construction once the order has been confirmed.
+	  </p>
+		<div class="subheading">
+			<h2 class="blue underscore">
+				RECEIVING THE ORDER
+			</h2>
+		</div>
+	  <p>
+			When the order is nearing completion we will send an invoice for the remainder of the order, due for payment on the expected sending date.
+			If there are outstanding amounts from previous invoices unpaid we will contact you to address this.
+			To avoid account build-up, if your account balance reaches an unreasonable amount we may withhold further dresses until it is reduced.
+	  </p>
+		<div class="subheading">
+			<h2 class="blue underscore">
+				RETENTION OF TITLE (ROMALPA)
+			</h2>
+		</div>
+		<p>
+			Sally Eagle Bridal Limited retains ownership of all goods supplied until payment has been made in full for the goods.
+			The purchaser shall hold goods supplied by Sally Eagle Bridal Ltd in the capacity as fiduciary owner.
+		</p>
+		<div class="subheading">
+			<h2 class="blue underscore">
+				RUSH ORDERS
+			</h2>
+		</div>
+		<p>
+			<em>The following applies in our busy season&#8212;between December and February.</em>
+		</p>
+		<p>
+			If a dress is required sooner than 12 weeks from the date of the order please contact us and enquire as to whether we can complete the order.
+			We are likely to be able to complete it, but we must factor in our production capacity and stock availability before we can accept the order.
+		</p>
+		<p>
+			If we can complete the order by the date required we will confirm our acceptance.
+			However if there are any potential issues we will advise you of the situation.
+			We will provide options for circumventing any issues and will only decline an order if we cannot overcome the issues.
+		</p>
+		<p>
+			We may charge a rush order fee at our discretion, which we will notify you of.
+		</p>
+		<div class="subheading">
+			<h2 class="blue underscore">
+				RETURNS
+			</h2>
+		</div>
+		<p>
+			In the situation where a dress arrives damaged or it is not the dress that you ordered you must advise us <b>within 7 days</b> of receiving it.
+			If the issue cannot be solved locally (or it is decided that that is not the best option) we will arrange for it to be returned.
+			We must be advised of the issue before you take any action.
+		</p>
+		<p>
+			If there is a problem with the dress that could only be discovered during the fitting, such as an incorrect construction, you have another 7 days from the date of the fitting to advise us.
+			Note that all dresses are provided as set sizing and some alterations are expected to be required in order to fit the dress to the bride.
+		</p>
+	</div>
+	<!-- END POLICY -->
+<?php include('templates/footer.php'); ?>
+</body>
+
+</html>
