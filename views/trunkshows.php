62f470c476e2da91b00afffc72626227fc7528b4
Karl Eagle <karleagle@gmail.com>
2018-03-06 16:13:49 +1300

    Remove Google Tag Manager

--- views/trunkshows.php
+++ views/trunkshows.php
@@ -19,14 +19,6 @@ $block = new Block('c','5')
 <script src="https://use.typekit.net/vyq5cic.js"></script>
 <script>try{Typekit.load({ async: true });}catch(e){}</script>
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NGJ5HZ');</script>
-<!-- End Google Tag Manager -->
-
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script type="text/javascript" src="js/menu_hover.js"></script>
 
